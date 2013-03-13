5a6c426204f9772605ecd6b258b816b2e3cb0e29
Elliott Landsborough <elliott.landsborough@gmail.com>
2013-03-13 19:14:24 +0000

    codeigniter 2.1.3 and html5bp 4.1.0

---
+++ elliott-ci/application/controllers/welcome.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('header');
+		//$this->load->view('welcome_message');
+		$this->load->view('footer');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
