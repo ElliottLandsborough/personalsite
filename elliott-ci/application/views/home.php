f1a69ce5478235cf40842ebc67ab2f0e8d29c2aa
Elliott Landsborough <elliott.landsborough@gmail.com>
2013-03-21 11:14:49 +0600

    images

--- elliott-ci/application/views/home.php
+++ elliott-ci/application/views/home.php
@@ -1,10 +1,14 @@
 <h2>About me</h2>
-<p>I'm an experienced website developer and server administrator based near London.</p>
-<h2>What I can do</h2>
-<ul>
-	<li>HTML, CSS, Javascript, Jquery/Zepto, AJAX, SASS</li>
-	<li>PHP, MySQL/Mariadb, Codeigniter, Wordpress, Node</li>
-	<li>Debian/Redhat, Apache, Nginx, Bash</li>
-</ul>
-<h2>More info</h2>
-<p>If you need a more detailed description of my skills you can <a href="<?php echo base_url('docs/CV.Elliott.Landsborough.public.pdf'); ?>">download my CV</a>.</p>
+<div itemscope itemtype="http://data-vocabulary.org/Person">
+	<p class="hidden">Hello, my name is <span itemprop="name">Elliott Landsborough</span></p>
+	<img src="/img/" />
+	<p>I'm an experienced website <span itemprop="title">developer</span> and server administrator based near London.</p>
+	<h2>What I can do</h2>
+	<ul>
+		<li>HTML, CSS, Javascript, Jquery/Zepto, AJAX, SASS</li>
+		<li>PHP, MySQL/Mariadb, Codeigniter, Wordpress, Node</li>
+		<li>Debian/Redhat, Apache, Nginx, Bash</li>
+	</ul>
+	<h2>More info</h2>
+	<p>If you need a more detailed description of my skills you can <a href="<?php echo base_url('docs/CV.Elliott.Landsborough.public.pdf'); ?>">download my CV</a>.</p>
+</div>	
