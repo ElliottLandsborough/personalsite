32262656554a2c02589c287a29a9d62923696006
Elliott Landsborough <elliott.landsborough@gmail.com>
2013-03-21 11:51:49 +0600

    structured data

--- elliott-ci/application/views/home.php
+++ elliott-ci/application/views/home.php
@@ -1,6 +1,6 @@
 <h2>About me</h2>
 <div itemscope itemtype="http://data-vocabulary.org/Person">
-	<p class="hidden">Hello, my name is <span itemprop="name">Elliott Landsborough</span></p>
+	<p class="hidden">Hello, my name is <a href="https://plus.google.com/108325287078081298995?rel=author" itemprop="name">Elliott Landsborough</a></p>
 	<p class="hidden"><img itemprop="photo" style="width: 5%; height: 5%;" src="/img/portrait.scaled.png" /></p>
 	<p>I'm an experienced <span itemprop="title">web developer</span> and server administrator based near <span itemprop="locality">London</span>.</p>
 	<h2>What I can do</h2>
