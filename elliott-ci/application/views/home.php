47ace81c7f12c22482f5dedf092054c97f273e40
Elliott Landsborough <elliott.landsborough@gmail.com>
2013-03-21 10:26:17 +0600

    update skills

--- elliott-ci/application/views/home.php
+++ elliott-ci/application/views/home.php
@@ -2,10 +2,9 @@
 <p>I'm an experienced website developer and server administrator based near London.</p>
 <h2>What I can do</h2>
 <ul>
-<li>HTML, CSS, Javascript, Jquery, AJAX</li>
-<li>PHP, MySQL, NodeJS, Codeigniter, Wordpress</li>
-<li>Linux (mainly Debian &amp; Redhat), Windows, Apache, Nginx, MySQL, PHP</li>
-<li>Cross Browser Compatibility, Photoshop, SEO, E-Commerce</li>
+	<li>HTML, CSS, Javascript, Jquery/Zepto, AJAX, SASS</li>
+	<li>PHP, MySQL/Mariadb, Codeigniter, Wordpress, Node</li>
+	<li>Debian/Redhat, Apache, Nginx, Bash</li>
 </ul>
 <h2>More info</h2>
 <p>If you need a more detailed description of my skills you can <a href="<?php echo base_url('docs/CV.Elliott.Landsborough.public.pdf'); ?>">download my CV</a>.</p>
